d68eee50a5f56e4cdac15290a929c0fa86b03473
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2020-04-15 18:59:58 +1000

    Gyro Function Test
    The application used to test the function of the Gyro. Will later be changed to a master Function test

---
+++ NDM-Nurse-Decision-Making/Function Demos/GyroTest/Assets/Scripts/CamGyro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamGyro : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject camParent;
+    void Start()
+    {
+
+        camParent = new GameObject("CamParent");
+        camParent.transform.position = this.transform.position;
+        this.transform.parent = camParent.transform;
+        Input.gyro.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camParent.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        this.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+    }
+
+    public void ResetCamera()
+    {
+        this.transform.rotation = Quaternion.identity;
+    }
+}
