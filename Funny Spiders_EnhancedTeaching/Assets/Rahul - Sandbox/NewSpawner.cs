0c0008783beb65fbaf968149728659db8652b61e
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2020-04-25 18:38:02 +1000

    Revert "Merge branch 'master' of https://github.com/deakinshaun/vrapps-t1-2020"
    This reverts commit 3556fe5107339db75bb26be61b80ad37e05b9206, reversing
    changes made to 77429323390f638e88c6e17c076d02ec1c7cb730.

--- Funny Spiders_EnhancedTeaching/Assets/Rahul - Sandbox/NewSpawner.cs
+++ Funny Spiders_EnhancedTeaching/Assets/Rahul - Sandbox/NewSpawner.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Vuforia;
 using UnityEngine;
 
 public class NewSpawner : MonoBehaviour
@@ -12,9 +11,7 @@ public class NewSpawner : MonoBehaviour
     public GameObject Corner3Object;
     public GameObject Corner4Object;
     public GameObject Animal;
-    public bool active;
     private Vector3 randomCoordinate;
-    public Vector3 scalemult;
     Vector3 corner1;
     Vector3 corner2;
     Vector3 corner3;
@@ -30,13 +27,13 @@ public class NewSpawner : MonoBehaviour
         corner3 = new Vector3(origin.x - range.x, origin.y, origin.z + range.z);
         corner4 = new Vector3(origin.x + range.x, origin.y, origin.z + range.z);
         SpawnObjects();
-       
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public Color GizmosColor = new Color(0.5f, 0.5f, 0.5f, 0.2f);
@@ -49,20 +46,21 @@ public class NewSpawner : MonoBehaviour
 
     private void SpawnObjects()
     {
-       // scalemult = new Vector3(0.05f, 0.05f, 0.05f);
-        HandlePlacement(Corner1Object,corner1,scalemult);
-        HandlePlacement(Corner2Object,corner2, scalemult);
-        HandlePlacement(Corner3Object,corner3, scalemult);
-        HandlePlacement(Corner4Object,corner4, scalemult);
-        HandlePlacement(Animal,randomCoordinate, scalemult);
-    }
-
-    private void HandlePlacement(GameObject objecttoSpawn,Vector3 place,Vector3 scale)
-    {
+        // for (int i = 0; i < 3; i++)
+        //{
         GameObject spawnedObject;
-        spawnedObject = Instantiate(objecttoSpawn) as GameObject;
-        spawnedObject.transform.localScale = scale;
-        spawnedObject.transform.position = place;
-        spawnedObject.transform.parent = transform;
+        spawnedObject= Instantiate(Corner1Object) as GameObject;
+
+        spawnedObject.transform.position = corner1;
+        spawnedObject = Instantiate(Corner2Object) as GameObject;
+        spawnedObject.transform.position = corner2;
+        spawnedObject = Instantiate(Corner3Object) as GameObject;
+        spawnedObject.transform.position = corner3;
+        spawnedObject = Instantiate(Corner4Object) as GameObject;
+        spawnedObject.transform.position = corner4;
+        spawnedObject = Instantiate(Animal) as GameObject;
+        spawnedObject.transform.position = randomCoordinate;
+        // CurrentAnimal = spawnedObject;
+        // }
     }
 }
