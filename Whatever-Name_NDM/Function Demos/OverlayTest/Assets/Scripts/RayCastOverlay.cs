10978d675f4c0b0bc46420642c3ba0b39ea62c3d
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2020-05-07 04:09:31 +1000

    Overlay Integration
    In this push, I have uploaded a demo application that showcases the Overlay system, which uses a Raycast to detect when over an Object with a certain tag, and shows the related information with it. More information can be found on the video recording on Trello.

---
+++ Whatever-Name_NDM/Function Demos/OverlayTest/Assets/Scripts/RayCastOverlay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RayCastOverlay : MonoBehaviour
+{
+    private bool Overlay;
+    public Camera camera;
+    GameObject temporaryObject;
+    
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.tag == "Monitor")
+                {
+                    Overlay = hit.collider.gameObject.GetComponent<OverlayManager>().OverlayShown = true;
+                   temporaryObject = hit.collider.gameObject;
+                }
+                else if (temporaryObject != null)
+                {
+                    temporaryObject.GetComponent<OverlayManager>().OverlayShown = false;
+            }
+
+            }
+            
+        }
+    }
+}
