7fbdafacf992a09eb8cdbd1f0fe5a6331c1fc6ce
TBickley01 <tbickley@deakin.edu.au>
2020-05-09 00:00:46 +1000

    Added initial Gyroscope Artefact

---
+++ sit383-africa-red-jackets-project-files/GyroscopeArtefact/Assets/Scripts/PhoneSensor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneSensor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion gyroAndroid = Input.gyro.attitude;
+        transform.rotation = new Quaternion(gyroAndroid.x, gyroAndroid.y, -gyroAndroid.z, -gyroAndroid.w);
+    }
+}
