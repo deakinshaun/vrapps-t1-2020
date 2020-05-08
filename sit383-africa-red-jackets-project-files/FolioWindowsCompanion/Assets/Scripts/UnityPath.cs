337f189cd208d788bf8dfef856f77dfa132e34e0
Hayden <hrydell@deakin.edu.au>
2020-05-08 11:52:00 +1000

    Added Windows companion program, deleted old Project Files, added .gitignore to repo

---
+++ sit383-africa-red-jackets-project-files/FolioWindowsCompanion/Assets/Scripts/UnityPath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class UnityPath : MonoBehaviour
+{
+	public string unityPath = "<no path>";
+	
+	public void onClick()
+	{
+		selectFile();
+	}
+	
+	public void selectFile()
+	{
+		unityPath = EditorUtility.OpenFilePanel("Select Unity executable","","");
+		Debug.Log("Folio will be created with: \n" + unityPath);
+		
+		if(unityPath.Length > 0)
+		{
+			Text _unityPath = GameObject.Find("Canvas/TxtUnityPath").GetComponent<Text>();
+			_unityPath.text = "Unity.exe Path: " + unityPath;
+		}
+	}
+}
---
+++ sit383-africa-red-jackets-project-files/GalleryNetworking/Assets/Scripts/FolioBundle/BundleCreate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class BundleCreate : MonoBehaviour
+{
+	public string 	user = "hrydell";			// temp
+	public int 		numOfArtefacts = 2;			// temp, update based on number of models uploaded by the user
+	
+	public void onClick() {
+		CreateAssetBundles();
+	}
+	
+	static void CreateAssetBundles() {
+		BuildPipeline.BuildAssetBundles("Assets/Folios", 
+			BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android); 	// builds bundle and compresses it. Can build target for StandaloneWindows or StandaloneWindows64
+		Debug.Log("Bundles built for Android platform");
+	}
+	
+/*	1. Need to get models in from an external location
+		Open model through explorer
+		Create list
+		Confirmation
+	2. Turn the collected models into a Bundle
+	3. Create a bundle
+	4. Store the bundle somewhere
+	5. Access the bundle between sessions
+	
+	Asset Bundles cannot be created during runtime.
+	
+
+*/
+	
+	
+}
