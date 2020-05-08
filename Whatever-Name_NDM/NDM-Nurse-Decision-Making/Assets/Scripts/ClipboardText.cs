985e7729e06d58e25523382120cef77d9b4e4971
RoflsMazoy <keithtanu@gmail.com>
2020-05-08 20:35:20 +1000

    Rudimentary History UI made
    Variables and a rudimentary UI for patient history has been made. Very basic, just to show that it's functional

---
+++ Whatever-Name_NDM/NDM-Nurse-Decision-Making/Assets/Scripts/ClipboardText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClipboardText : MonoBehaviour
+{
+    public string PatientName;
+    public string Gender;
+    public string PatientAge;
+    public string Allergies;
+    public string PatientHealthConditions;
+
+    public Text ClipboardTextbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClipboardTextbox.GetComponent<Text>();
+
+        ClipboardTextbox.text = "Name: " + PatientName + "\r\n" +
+                                "Gender: " + Gender + "\r\n" +
+                                "Age: " + PatientAge + "\r\n" +
+                                "Allergies: " + Gender + "\r\n" +
+                                "Health Conditions: " + Gender + "\r\n";
+    }
+
+}
