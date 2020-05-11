a1d80f0a315e4e7273b2aab57cb37b840d76970b
Parisa-Nafis <chaity.nafis@gmail.com>
2020-05-12 02:14:04 +1000

    Added a test scene, test Target Image with an interactive button to get the basic setup working, Added photon Appid and Vuforia image database. Added Photon Manager script although it's having trouble detecting the Photon.Pun library.

---
+++ Midnight_Oil/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    public GameObject avatarPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Photon manager starting");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected. Yay!");
+        RoomOptions roomopt = new RoomOptions();
+        PhotonNetwork.JoinOrCreateRoom("ApplicationRoom", roomopt, new TypedLobby("ApplicationLobby", LobbyType.Default));
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("You are in the room with " + PhotonNetwork.CurrentRoom.PlayerCount + " other participants. You see a grue.");
+        PhotonNetwork.Instantiate(avatarPrefab.name, new Vector3(), Quaternion.identity,0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Midnight_Oil/Assets/Scripts/TestButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class TestButton : MonoBehaviour, IVirtualButtonEventHandler
+{ 
+    public GameObject Obj;
+    public GameObject mat;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        mat.GetComponent<Renderer>().material.color= new Color (1f,1f,0f);
+        mat.transform.position += new Vector3(0f, -0.005f, 0f);
+        Debug.Log("ButtonPressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        mat.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+        mat.transform.position += new Vector3(0f, 0.005f, 0f);
+        Debug.Log("ButtonReleased");
+    } 
+
+    void Start ()
+    {
+        Obj = GameObject.Find("Button");
+
+        Obj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        
+
+    }
+}
