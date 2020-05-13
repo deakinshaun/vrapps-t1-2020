6670d7595f09b073ef8d80d24a4d09d3a02da2fa
rmasakorala <rmasakorala@deakin.edu.au>
2020-05-13 13:33:02 +1000

    Updated movement to allow using Virtual pointer to select and tell animal where to go

---
+++ Funny Spiders_EnhancedTeaching/Assets/Rahul - Sandbox/AnimalMovement.cs
@@ -0,0 +1,157 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public Vector3 boundary;
+    private Vector3 Origin;
+    private Vector3 Destination;
+    public GameObject target;
+    public float speed = 0.0f;
+    public float Rspeed = 0.0f;
+    public float tolerance = 2;
+
+    public int waitTime = 0;
+    //public Text debugText;
+
+    public bool hasTarget = false;
+    private bool timer = false;
+
+    public bool isMoving = true;
+    void Start()
+    {
+
+        Origin = transform.position;
+
+
+        Destination = RandomPointInBox(Origin, boundary);
+
+        StartCoroutine(DestinationWait());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  debugText.text = (" " + timer);
+
+
+        if (hasTarget)
+        {
+            MoveTo(target);
+        }
+        else
+        {
+            if (timer == true)
+            {
+                if (Vector3.Distance(transform.position, Destination) < tolerance)
+                {
+
+                    Invoke("getNewDestination", 1);
+                    timer = false;
+
+                }
+            }
+            MoveTo(Destination);
+        }
+
+
+
+    }
+
+    public void MoveTo(GameObject target)
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+
+        Vector3 targetDirection = target.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = Rspeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+
+        if (Vector3.Distance(transform.position, target.transform.position) < tolerance)
+        {
+            isMoving = false;
+            hasTarget = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+
+        if (isMoving == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
+
+    }
+    public void MoveTo(Vector3 destination)
+    {
+
+        float step = speed * Time.deltaTime; // calculate distance to move
+
+        Vector3 targetDirection = destination - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = Rspeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, Destination) > tolerance)
+        {
+
+            timer = true;
+        }
+
+
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+
+    }
+    public void setTarget(GameObject newTarget)
+    {
+        target = newTarget;
+        hasTarget = true;
+    }
+
+    private static Vector3 RandomPointInBox(Vector3 center, Vector3 size)
+    {
+
+        return center + new Vector3(
+           (Random.value - 0.5f) * size.x,
+          0,
+           (Random.value - 0.5f) * size.z
+        );
+    }
+
+    public void getNewDestination()
+    {
+        Destination = RandomPointInBox(Origin, boundary);
+    }
+
+    IEnumerator DestinationWait()
+    {
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        while (Vector3.Distance(transform.position, target.transform.position) < tolerance)
+        {
+            yield return new WaitForSeconds(1);
+            getNewDestination();
+        }
+
+
+    }
+
+
+}
--- Funny Spiders_EnhancedTeaching/Assets/Rahul - Sandbox/Selection.cs
+++ Funny Spiders_EnhancedTeaching/Assets/Rahul - Sandbox/Selection.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Selection : MonoBehaviour
 {
     // Start is called before the first frame update
     private GameObject selectedObject;
+    private GameObject currentSelection;
+    private GameObject previousSelection;
     public Material selectedMat;
+    public AnimalMovement animalMovementScript;
+    private Material DefaultMat;
+    bool targetAquired;
+    public Text debugtext;
     void Start()
     {
         
@@ -15,15 +22,42 @@ public class Selection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((previousSelection.gameObject.tag == "Animal")&&(selectedObject.gameObject.tag == "Habitat"))
+        {
+            debugtext.text = "I HAVE A TARGET";
+            targetAquired = true;
+
+            animalMovementScript = previousSelection.GetComponent<AnimalMovement>();
+            if (!animalMovementScript.hasTarget)
+            {
+                animalMovementScript.setTarget(selectedObject);
+            }
+
+        }
+        else
+        {
+            debugtext.text = "Just another habitat";
+        }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-    }
+
     private void OnTriggerEnter(Collider other)
     {
         selectedObject = other.gameObject;
+        DefaultMat = selectedObject.GetComponent<Renderer>().material;
         selectedObject.GetComponent<Renderer>().material = selectedMat;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Deselect();
+    }
+
+    private void Deselect()
+    {
+   
+        previousSelection = selectedObject;
+        
+       
+        selectedObject.GetComponent<Renderer>().material = DefaultMat;
     }
 }
