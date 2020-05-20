4691fb7ddcab55b48da1c2a8cdfb6ea1285844f1
AndyBui <tcb@deakin.edu.au>
2020-05-20 20:24:37 +1000

    adding content for lesson

---
+++ Funny Spiders_EnhancedTeaching/Assets/Andy Scene/Gaze.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Gaze : MonoBehaviour
+{
+
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position,transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("Info"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+            }
+        }
+        else
+        {
+            CloseAll();
+        }
+    }
+
+    void OpenInfo(InfoBehavior desirerdInfo)
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            if(info == desirerdInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
---
+++ Funny Spiders_EnhancedTeaching/Assets/Andy Scene/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Tiger, Elephant, Star1;
+
+    private bool show = false;
+
+    public void ShowHideInfo()
+    {
+        if (!show)
+        {
+            Tiger.SetActive(false);
+            Elephant.SetActive(true);
+            Star1.SetActive(true);
+            show = true;
+        }
+    }
+}
