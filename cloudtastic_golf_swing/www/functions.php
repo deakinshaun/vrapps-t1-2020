9bccdcbcad9ce24860041a283fc05681f625ff37
geoff <geoff.newman@immersive.video>
2020-05-20 20:56:09 +1000

    continue php dev

--- cloudtastic_golf_swing/www/functions.php
+++ cloudtastic_golf_swing/www/functions.php
@@ -9,14 +9,30 @@
 		if (isset($_GET["VID"]) && isset($_GET["UID"]))
 		{
 			header("content-type: application/jsonS");
-			echo Connection($db, $_GET["VID"]);
+			echo getData($db, $_GET["VID"]);
 		}
 		return 0;
 	}
 
-	function Connection($db, $VID)
+	function getData($db, $VID)
 	{
 		$sql = "SELECT DATA FROM sharedata WHERE VID = '$VID'";
+		if ($db->query($sql) === FALSE) {
+					echo("Error: " .  $db->error);
+				}
+			$result = $db -> query($sql);
+			
+		if ($result->num_rows > 0) return result;
+		else return "false";
+		// clear connected devices 
+	}
+	function setData($db)
+	{
+				$str=rand(); 
+		$result = md5($str); 
+		$sql = "INSERT INTO sharedata (VID, UID, DATA) = '$result'";
+
+
 				if ($db->query($sql) === FALSE) {
 					echo("Error: " .  $db->error);
 				}
