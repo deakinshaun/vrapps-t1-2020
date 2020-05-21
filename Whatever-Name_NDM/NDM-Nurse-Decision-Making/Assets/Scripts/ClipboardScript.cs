304e85ffb9517fe434c0aacccc241ecd918b5bf8
RoflsMazoy <keithtanu@gmail.com>
2020-05-21 17:21:34 +1000

    finished clipboard
    yes, clipboard goes up and down

--- Whatever-Name_NDM/NDM-Nurse-Decision-Making/Assets/Scripts/ClipboardScript.cs
+++ Whatever-Name_NDM/NDM-Nurse-Decision-Making/Assets/Scripts/ClipboardScript.cs
@@ -18,7 +18,8 @@ public class ClipboardScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ClipboardTextbox.text = "Name: " + PatientName + "\r\n" +
+        ClipboardTextbox.text = "Patient Information" + "\r\n" +
+                                "Name: " + PatientName + "\r\n" +
                                 "Gender: " + PatientGender + "\r\n" +
                                 "Age: " + PatientAge + "\r\n" +
                                 "Allergies: " + PatientAllergies + "\r\n" +
@@ -26,9 +27,46 @@ public class ClipboardScript : MonoBehaviour
 
     }
 
-    void Popup ()
+    private void PopupTrue ()
     {
         PopupEnabled = true;
         Debug.Log("Popup Enabled =" + PopupEnabled);
+    }
+    private void PopupFalse ()
+    {
+    PopupEnabled = false;
+    Debug.Log("Popup Enabled =" + PopupEnabled);
+    }
+
+    public void Popup ()
+    {
+        if (PopupEnabled == false)
+        {
+            Invoke("PopupTrue", 0.1f);
+        }
+
+        if (PopupEnabled == true)
+        {
+            Invoke("PopupFalse", 0.1f);
+        }
+    }
+
+    private void Update()
+    {
+        if (PopupEnabled == true)
+        {
+            if (transform.position.y < -1.0f)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            }
+        }
+
+        if (PopupEnabled == false)
+        {
+            if (transform.position.y > -1.55f)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+            }
+        }
     }
 }
