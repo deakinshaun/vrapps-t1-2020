18956ed9a88a158c91e2c683aea7ab05fce6e5d3
ssagolj <ssagolj@deakin.edu.au>
2020-05-23 21:21:02 +1000

    Artefacts and button refinement
    Created three artefacts that will be displayed within the gallary along with refining the virtual buttons

---
+++ sit383-africa-red-jackets-project-files/Project Files/Assets/Scripts/ColDisplayHistory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class ColDisplayHistory : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject virtualColButton;
+    public TextMesh colHistoryLog;
+    bool ButtonOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualColButton = GameObject.Find("ColourHistButton");
+        virtualColButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        TextMesh colHistoryLog = GameObject.Find("ColHistoryLog").GetComponent<TextMesh>();
+        colHistoryLog.gameObject.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (ButtonOn)
+        {
+            ButtonOn = false;
+            colHistoryLog.gameObject.SetActive(false);
+        }
+        else
+        {
+            ButtonOn = true;
+            colHistoryLog.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        // Debug.Log("Button Released");
+    }
+
+}
